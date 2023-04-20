c51195e0863d48319aaf9ac615451347c4df4336
Christy <christyelzey@gmail.com>
2023-04-20 16:57:08 -0700

    Continued to update the comic component

--- src/app/Components/Comic.tsx
+++ src/app/Components/Comic.tsx
@@ -1,35 +1,39 @@
 import Image from "next/image"
 import styles from '../page.module.css'
+import comicStyles from "./Comics.module.css" 
 import { useState } from "react"
 import Detail from "./Subcomponents/Detail"
+import classnames from "classnames"
 
 export default function Comic( props : Issue ) {
-    // const [ isFavorite, setIsFavorite ] = useState(false);
-    const { title, issueNumber, publishDate, creators, thumbnail } = props;
-    let creatorsDetail = ""
-    creators.map(creator => {
-      creatorsDetail += creator.name + ", "; 
-      }
-    )
+  // const [ isFavorite, setIsFavorite ] = useState(false);
+  const { title, issueNumber, publishDate, creators, thumbnail } = props;
+  let creatorsDetail = ""
+  creators.map(creator => {
+    creatorsDetail += creator.name + ", "; 
+    }
+  )
 
-    const options = { 
-      month: "long",
-      day: "numeric",
-      year: "numeric" };
-    const pubDate = new Date(publishDate);
-    const formattedDate = new Intl.DateTimeFormat("en-US", options).format(pubDate);
+  const options = { 
+    month: "long",
+    day: "numeric",
+    year: "numeric" };
+  const pubDate = new Date(publishDate);
+  const formattedDate = new Intl.DateTimeFormat("en-US", options).format(pubDate);
 
 
-    return(
-      <div className={styles.card}>
-        <Image src={thumbnail} width={183} height={276} alt="" />
+  return(
+    <div className={classnames(styles.card, comicStyles.card)}>
+      <Image src={thumbnail} width={183} height={276} alt="" />
+      <div>
         <h3>{title}</h3>
         <Detail tag="Issue" info={issueNumber} />
         <Detail tag="Published" info={formattedDate} />
         <Detail tag="Creators" info={creatorsDetail.slice(0, -2)} />
       </div>
-    )
-  }
+    </div>
+  )
+}
 
 interface Issue {
   title : string
